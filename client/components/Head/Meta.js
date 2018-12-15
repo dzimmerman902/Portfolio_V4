8825b13cb2d7d8399706db891b1eec3a71a87440
Devin Zimmerman <dzimmerman902@gmail.com>
2018-12-14 18:45:28 -0700

    Starter Files

---
+++ client/components/Head/Meta.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-wdith, initial-scale=1" />
+    <meta charSet="utf-8" />
+
+    <title>Devin Zimmerman</title>
+  </Head>
+)
